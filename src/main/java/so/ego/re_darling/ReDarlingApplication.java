98995795607bd221dc57362002ebec1608f13682
aejeong <aejeong-context@kakao.com>
2021-12-02 22:27:44 +0900

    [init] Project redevelopment

---
+++ src/main/java/so/ego/re_darling/ReDarlingApplication.java
@@ -0,0 +1,13 @@
+package so.ego.re_darling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReDarlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReDarlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/so/ego/re_darling/ReDarlingApplicationTests.java
@@ -0,0 +1,13 @@
+package so.ego.re_darling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReDarlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
