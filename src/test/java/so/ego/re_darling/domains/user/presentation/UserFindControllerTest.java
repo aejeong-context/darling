da99cf0513d3e515ad8f02729b1d49ecb5fb20b8
aejeong <aejeong-context@kakao.com>
2022-02-01 00:15:45 +0900

    [add] User login, check 기능 TestCode 추가
    - login Test 시 ResponseField 오류
    -> Cannot document response fields as the response body is empty

---
+++ src/test/java/so/ego/re_darling/domains/user/presentation/UserFindControllerTest.java
@@ -0,0 +1,99 @@
+package so.ego.re_darling.domains.user.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import so.ego.re_darling.domains.user.application.UserFindService;
+import so.ego.re_darling.domains.user.application.UserRegisterService;
+import so.ego.re_darling.domains.user.application.dto.UserFindResponse;
+import so.ego.re_darling.domains.user.application.dto.UserLoginRequest;
+import so.ego.re_darling.domains.user.application.dto.UserLoginResponse;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest
+class UserFindControllerTest {
+  private MockMvc mockMvc;
+
+  @MockBean private UserFindService userFindService;
+  @Autowired protected ObjectMapper objectMapper;
+
+  @BeforeEach
+  public void setUp(
+      WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation) {
+    this.mockMvc =
+        MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .build();
+  }
+
+  @Test
+  void checkUser() throws Exception {
+    final UserFindResponse userFindResponse = UserFindResponse.builder().result(true).build();
+    when(userFindService.findUser(anyString())).thenReturn(userFindResponse);
+
+    this.mockMvc
+        .perform(get("/user/check/{socialToken}", "abc").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document(
+                "user-find-check",
+                pathParameters(parameterWithName("socialToken").description("소셜토큰")),
+                responseFields(fieldWithPath("result").description("회원 조회 결과"))));
+  }
+  // TODO: 2022/02/01 ResponseField 오류
+  @Test
+  void loginUser() throws Exception {
+    final UserLoginRequest userLoginRequest =
+        UserLoginRequest.builder().socialToken("Asd823daz").pushToken("Wesdf0898dfa").build();
+    final UserLoginResponse userLoginResponse =
+        UserLoginResponse.builder().coupleToken("7E0U7W").socialToken("Asd823daz").build();
+
+    when(userFindService.loginUser(userLoginRequest)).thenReturn(userLoginResponse);
+
+    this.mockMvc
+        .perform(
+            post("/login")
+                .content(this.objectMapper.writeValueAsString(userLoginRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document(
+                "user-login",
+                requestFields(
+                    fieldWithPath("socialToken").description("소셜 토큰"),
+                    fieldWithPath("pushToken").description("푸시 토큰").optional()),
+                responseFields(
+                    fieldWithPath("socialToken").description("소셜 토큰"),
+                    fieldWithPath("coupleToken").description("커플 토큰"))));
+  }
+}
