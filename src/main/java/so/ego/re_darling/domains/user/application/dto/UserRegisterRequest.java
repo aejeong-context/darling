cb74f1434c6491a5933c65de9bd2eb0078a0fc25
aejeong <aejeong-context@kakao.com>
2022-02-01 00:14:36 +0900

    [mod] UserRegister Test를 위한 Builder 적용
    - TEST 를 위해 @Builder를 사용

--- src/main/java/so/ego/re_darling/domains/user/application/dto/UserRegisterRequest.java
+++ src/main/java/so/ego/re_darling/domains/user/application/dto/UserRegisterRequest.java
@@ -1,8 +1,10 @@
 package so.ego.re_darling.domains.user.application.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserRegisterRequest {
 
     private String socialToken;
