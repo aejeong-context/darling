085256d9fd5bea58cffc4ce89429989d56a05880
aejeong <aejeong-context@kakao.com>
2022-02-28 14:37:28 +0900

    [mod] package 변경
    유지보수 위해 package 변경

--- src/main/java/so/ego/re_darling/domains/user/application/CoupleResisterService.java
+++ src/main/java/so/ego/re_darling/domains/user/application/CoupleResisterService.java
@@ -1,44 +1,44 @@
-//package so.ego.re_darling.domains.user.application;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//import so.ego.re_darling.domains.user.domain.Couple;
-//import so.ego.re_darling.domains.user.domain.CoupleRepository;
-//
-//import java.time.LocalDateTime;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class CoupleResisterService {
-//
-//  private final CoupleRepository coupleRepository;
-//
-//  @Transactional
-//  public Couple addCouple() {
-//    String coupleToken = createToken();
-//    int coupleCount = coupleRepository.countByCoupleToken(coupleToken);
-//    if (coupleCount == 0) {
-//      return coupleRepository.save(
-//          Couple.builder().coupleToken(coupleToken).firstDay(LocalDateTime.now()).build());
-//    } else {
-//      return addCouple();
-//    }
-//  }
-//
-//  public String createToken() {
-//    String token = "";
-//
-//    for (int i = 0; i < 6; i++) {
-//      int r = (int) ((Math.random() * 2) + 1); // 숫자로할지 영어로 할지 랜덤
-//      char d = (char) ((Math.random() * 10) + 48); // '1' , '2' , '3'~ '9'
-//      char c = (char) ((Math.random() * 26) + 65); // 대문자
-//      if (r != 1) {
-//        token = token + d;
-//      } else {
-//        token = token + c;
-//      }
-//    }
-//    return token;
-//  }
-//}
+package so.ego.re_darling.domains.user.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import so.ego.re_darling.domains.user.domain.Couple;
+import so.ego.re_darling.domains.user.domain.CoupleRepository;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class CoupleResisterService {
+
+  private final CoupleRepository coupleRepository;
+
+  @Transactional
+  public Couple addCouple() {
+    String coupleToken = createToken();
+    int coupleCount = coupleRepository.countByCoupleToken(coupleToken);
+    if (coupleCount == 0) {
+      return coupleRepository.save(
+          Couple.builder().coupleToken(coupleToken).firstDay(LocalDateTime.now()).build());
+    } else {
+      return addCouple();
+    }
+  }
+
+  public String createToken() {
+    String token = "";
+
+    for (int i = 0; i < 6; i++) {
+      int r = (int) ((Math.random() * 2) + 1); // 숫자로할지 영어로 할지 랜덤
+      char d = (char) ((Math.random() * 10) + 48); // '1' , '2' , '3'~ '9'
+      char c = (char) ((Math.random() * 26) + 65); // 대문자
+      if (r != 1) {
+        token = token + d;
+      } else {
+        token = token + c;
+      }
+    }
+    return token;
+  }
+}
